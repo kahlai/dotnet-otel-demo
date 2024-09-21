310fe345c7a54774bc7cd32100e56ca8e2a45358
Lai Kah Hoe <kahlai@redhat.com>
2024-09-21 19:00:43 +0800

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,102 @@
 using System.Globalization;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+const string serviceName = "roll-dice";
+
+Console.Out.WriteLine("Otlp:Endpoint : "  + builder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddConsoleExporter()
+        .AddOtlpExporter();
+});
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(serviceName))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddConsoleExporter()
+          .AddOtlpExporter(
+            options =>
+            {
+                //options.Endpoint = new Uri("http://localhost:4317/v1/traces");
+                options.Endpoint = new Uri(builder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
+                options.Protocol = OtlpExportProtocol.Grpc;
+            }
+          ))
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddConsoleExporter()
+          .AddOtlpExporter());
+
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddOpenTelemetry()
+//     .WithTracing(tracing => tracing
+//         // The rest of your setup code goes here
+//         .AddOtlpExporter(options =>
+//         {
+//             options.Endpoint = new Uri("http://localhost:4317/v1/traces");
+//             options.Protocol = OtlpExportProtocol.HttpProtobuf;
+//         }))
+//     .WithMetrics(metrics => metrics
+//         // The rest of your setup code goes here
+//         .AddOtlpExporter(options =>
+//         {
+//             options.Endpoint = new Uri("http://localhost:4317/v1/metrics");
+//             options.Protocol = OtlpExportProtocol.HttpProtobuf;
+//         }));
+
+// builder.Logging.AddOpenTelemetry(logging => {
+//     // The rest of your setup code goes here
+//     logging.AddOtlpExporter(options =>
+//     {
+//         options.Endpoint = new Uri("http://localhost:4317/v1/logs");
+//         options.Protocol = OtlpExportProtocol.HttpProtobuf;
+//     });
+// });
+
+// var tracerProvider = Sdk.CreateTracerProviderBuilder()
+//     // Other setup code, like setting a resource goes here too
+//     .AddOtlpExporter(options =>
+//     {
+//         options.Endpoint = new Uri("http://localhost:4317/v1/traces");
+//         options.Protocol = OtlpExportProtocol.Grpc;
+//     })
+//     .Build();
+
+// var meterProvider = Sdk.CreateMeterProviderBuilder()
+//     // Other setup code, like setting a resource goes here too
+//     .AddOtlpExporter(options =>
+//     {
+//         options.Endpoint = new Uri("http://localhost:4317/v1/metrics");
+//         options.Protocol = OtlpExportProtocol.Grpc;
+//     })
+//     .Build();
+
+// var loggerFactory = LoggerFactory.Create(builder =>
+// {
+//     builder.AddOpenTelemetry(logging =>
+//     {
+//         logging.AddOtlpExporter(options =>
+//         {
+//             options.Endpoint = new Uri("http://localhost:4317/v1/logs");
+//             options.Protocol = OtlpExportProtocol.Grpc;
+//         });
+//     });
+// });
+
 var app = builder.Build();
 
 var logger = app.Logger;
--- obj/Debug/net8.0/otel.AssemblyInfo.cs
+++ obj/Debug/net8.0/otel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("otel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a68e72bba40b6947ffe297c0533a0daaf50aa05")]
 [assembly: System.Reflection.AssemblyProductAttribute("otel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("otel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
